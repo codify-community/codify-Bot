04f50b98614ee23dfbacf2c2f72b33a66f351dbc
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2024-07-10 17:56:04 -0300

    feat: Melhorando o visual do embed de top bumpers

--- src/use_cases/general/top_bumpers.py
+++ src/use_cases/general/top_bumpers.py
@@ -18,22 +18,18 @@ class TopBumpersUseCase(UseCase):
             embed.description = "Ninguém bumpou ainda!"
             return await self.send_message(embed=embed)
 
-        embed = Embed(title="Top bumpers", color=0x9F6CFD, description="")
+        embed = Embed(title="🏆 Top Bumpers", color=0x9F6CFD, description="")
         embed.set_thumbnail(url=config["guild"]["icon"])
 
         for place, user in enumerate(top_bumpers, start=1):
             isUserInGuild = guild.get_member(user["_id"])
-            if isUserInGuild is not None:
+            if isUserInGuild:
                 mention = isUserInGuild.mention
             else:
                 current_user = await client.fetch_user(user["_id"])
-                mention = current_user.name
+                mention = current_user.name.capitalize()
 
             embed.description += f"**{place}º** {mention} com {user['bumpCount']} {'bumps' if user['bumpCount'] > 1 else 'bump'}!\n"
 
-        embed.set_footer(
-            text=datetime.now().strftime("%d/%m/%Y as %H:%M:%S"),
-            icon_url=self.author.guild.icon.url,
-        )
-
+        embed.set_footer(text=datetime.now().strftime("%d/%m/%Y as %H:%M"))
         await self.send_message(embed=embed)
